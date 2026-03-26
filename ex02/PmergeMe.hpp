a04135f9c610aa56d696d8c3fb362d3a2271251d
flash <august1.caes@gmail.com>
2026-03-26 15:18:24 +0100

    fix little things

--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -7,29 +7,22 @@
 
 class PmergeMe
 {
-private:
-	std::vector<int>	_vec;
-	std::deque<int>		_deq;
-
-	// Ford-Johnson pour vector
-	void	_sortVector(std::vector<int> &seq);
-	void	_insertVector(std::vector<int> &main, const std::vector<int> &pend);
-
-	// Ford-Johnson pour deque
-	void	_sortDeque(std::deque<int> &seq);
-	void	_insertDeque(std::deque<int> &main, const std::deque<int> &pend);
-
-	// Generateur de nombres de Jacobsthal
-	std::vector<int>	_jacobsthal(int n) const;
-
-public:
-	PmergeMe();
-	PmergeMe(const PmergeMe &other);
-	PmergeMe	&operator=(const PmergeMe &other);
-	~PmergeMe();
-
-	bool	parseInput(int argc, char **argv);
-	void	run();
+    private:
+        std::vector<int>	_vec;
+        std::deque<int>		_deq;
+        void	_sortVector(std::vector<int> &seq);
+        void	_insertVector(std::vector<int> &main, const std::vector<int> &pend);
+        void	_sortDeque(std::deque<int> &seq);
+        void	_insertDeque(std::deque<int> &main, const std::deque<int> &pend);
+        std::vector<int>	_jacobsthal(int n) const;
+    
+    public:
+        PmergeMe();
+        PmergeMe(const PmergeMe &other);
+        PmergeMe	&operator=(const PmergeMe &other);
+        ~PmergeMe();
+        bool	parseInput(int argc, char **argv);
+        void	run();
 };
 
 #endif
